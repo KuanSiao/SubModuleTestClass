fdd7536a56f521a4178a0e637e03613f6271acb9
CSF-AD\kuan_siao <kuan_siao@mail.csf.org.tw>
2025-04-22 09:35:08 +0800

    計算age

--- MyCommonLib/TestMethod.cs
+++ MyCommonLib/TestMethod.cs
@@ -4,7 +4,16 @@
     {
         public int Add(int a, int b) => a + b;
         public string GetString(string str) => $"Hello {str}";
-        public string GetAddress(string address) => $"Address: {address}";  
+        public string GetAddress(string address) => $"Address: {address}";
+        public string GetAgeByYear(DateTime birthDate)
+        {
+            DateTime today = DateTime.Today;
+            int age = today.Year - birthDate.Year;
+            // 若生日還沒過，要減一歲
+            if (birthDate.Date > today.AddYears(-age)) age--;
+
+            return $"You are {age} years old.";
+        }
     }
 }
 
